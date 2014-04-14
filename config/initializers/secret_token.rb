a42fc478dffd6b28fc00a8ac8ec9de38404f0249
Alex Hawkins <alexhawkins.me@gmail.com>
2014-04-14 13:40:59 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Focalist::Application.config.secret_key_base = 'b76f971ee286cdeb7b09c48e5f602e115bf4032c41de6e3f43633e4d5ab4f4f84b12fb9a4a87c560444a91a8195eb0a08afde21fc9c876ca4ad9857388684444'
