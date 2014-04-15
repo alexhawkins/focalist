6757eaa4e9d1f7605c679c2f93b752eefa95d436
Alex Hawkins <alexhawkins.me@gmail.com>
2014-04-15 20:39:27 +0000

    Add create and update CRUD actions for List objects

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,8 +8,36 @@ class ListsController < ApplicationController
   end
 
   def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list, notice: 'Your new LIST was saved'
+    else
+      flash[:error] = "There was an error saving the list. Please try again"
+      render :new
+    end
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update_attributes(list_params)
+      redirect_to @list, notice: 'List updated'
+    else
+      flash[:error] = "There was an error updating the list. Please try again"
+      render :edit
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
