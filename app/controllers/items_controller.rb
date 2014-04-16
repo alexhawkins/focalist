e75f21f2d8656bb0e9d48415e547aceb01dccc9f
Alex Hawkins <alexhawkins.me@gmail.com>
2014-04-16 23:11:12 +0000

    Ajaxify item creation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  #enable the controller to be able to respond to html and javascript
   respond_to :html, :js
 
   def index
@@ -23,26 +24,34 @@ class ItemsController < ApplicationController
 
     if @item.save
        flash[:notice] = "Your new ITEM was saved"
-       redirect_to @list  #=> redirect_to list_path(@list)
+       #redirect_to @list  #=> redirect_to list_path(@list)
     else
       flash[:error] = "There was an error saving the item. Please try again"
-      render @list
+      #render @list
       #render :new
     end
+     #this overites the default behavior and instructs the controller to respond via Ajax
+     respond_with(@item) do |f|
+     f.html { redirect_to [@list] }
+    end
+
   end
 
   def destroy
     @list = List.find(params[:list_id])
-    @item = Item.find(params[:id])
+    @item = @list.items.find(params[:id])
 
     if @item.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to @list
+      #redirect_to @list
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to @list
+      #redirect_to @list
     end
-
+    #this overites the default behavior and instructs the controller to respond via Ajax
+    respond_with(@item) do |f|
+     f.html { redirect_to @list }
+    end
   end
 
   def complete
